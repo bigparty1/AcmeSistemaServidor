deb739ac4984ca3584be762cea74f695579abbf1
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-13 18:54:59 -0300

    feat: Adicionar classe Tratamento

---
+++ Data/Models/Entities/Tratamento.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcmeSistemaServidor.Data.Models.Entities
+{
+    public class Tratamento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdTratamento { get; set; }
+
+        [ForeignKey(nameof(Paciente))]
+        public int IdPaciente { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+
+        public Paciente? Paciente { get; set; }
+    }
+}
