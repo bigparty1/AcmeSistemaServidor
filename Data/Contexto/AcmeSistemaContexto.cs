95cecab95020455e04092be15a0cf63309b66f25
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-13 19:07:59 -0300

    feat: Adicionar classe AcmeSistemaContexto

---
+++ Data/Contexto/AcmeSistemaContexto.cs
@@ -0,0 +1,32 @@
+﻿using AcmeSistemaServidor.Data.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcmeSistemaServidor.Data.Contexto
+{
+    public class AcmeSistemaContexto : DbContext
+    {
+        public AcmeSistemaContexto(DbContextOptions<AcmeSistemaContexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Tratamento> Tratamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //cpf unico
+            modelBuilder.Entity<Paciente>()
+                .HasIndex(p => p.CPF)
+                .IsUnique();
+
+            //index Paciente.Nome
+            modelBuilder.Entity<Paciente>()
+                .HasIndex(p => p.Nome);
+
+            //datetime with timezone (postgreSQL)
+            modelBuilder.Entity<Tratamento>()
+                .Property(t => t.Date)
+                .HasColumnType("timestamp with time zone");
+        }
+    }
+}
