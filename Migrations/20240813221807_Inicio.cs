6affb178e077b0b3da209879c8153abb0c3c8ec0
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-13 19:19:28 -0300

    feat: Adicionar migração inicial do banco de dados

---
+++ Migrations/20240813221807_Inicio.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AcmeSistemaServidor.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    IdPaciente = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Nascimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    CPF = table.Column<string>(type: "text", nullable: false),
+                    Sexo = table.Column<int>(type: "integer", nullable: false),
+                    CEP = table.Column<string>(type: "text", nullable: false),
+                    Cidade = table.Column<string>(type: "text", nullable: false),
+                    Bairro = table.Column<string>(type: "text", nullable: false),
+                    Rua = table.Column<string>(type: "text", nullable: false),
+                    Complemento = table.Column<string>(type: "text", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.IdPaciente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tratamentos",
+                columns: table => new
+                {
+                    IdTratamento = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdPaciente = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tratamentos", x => x.IdTratamento);
+                    table.ForeignKey(
+                        name: "FK_Tratamentos_Pacientes_IdPaciente",
+                        column: x => x.IdPaciente,
+                        principalTable: "Pacientes",
+                        principalColumn: "IdPaciente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_CPF",
+                table: "Pacientes",
+                column: "CPF",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_Nome",
+                table: "Pacientes",
+                column: "Nome");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tratamentos_IdPaciente",
+                table: "Tratamentos",
+                column: "IdPaciente");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tratamentos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
