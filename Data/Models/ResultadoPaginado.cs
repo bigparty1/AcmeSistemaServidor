39a7d0ffc6945d0ebf894c371ed76afaef8a43d7
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 09:11:52 -0300

    feat: Adicionar classe ResultadoPaginado para paginação

---
+++ Data/Models/ResultadoPaginado.cs
@@ -0,0 +1,10 @@
+﻿namespace AcmeSistemaServidor.Data.Models
+{
+    public class ResultadoPaginado <T>
+    {
+        public required ICollection<T> Items { get; set; }
+        public int TotalItems { get; set; }
+        public int PaginaAtual { get; set; }
+        public int TotalPorPaginas { get; set; }
+    }
+}
