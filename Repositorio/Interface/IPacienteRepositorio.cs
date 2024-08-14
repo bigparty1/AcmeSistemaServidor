13fb5b07b799de17eea657c8dabbce972df48544
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 09:35:51 -0300

    feat: Adicionar interface do repositório do paciente

---
+++ Repositorio/Interface/IPacienteRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using AcmeSistemaServidor.Data.Models;
+using AcmeSistemaServidor.Data.Models.Entities;
+
+namespace AcmeSistemaServidor.Repositorio.Interface
+{
+    public interface IPacienteRepositorio
+    {
+        public Task<ResultadoPaginado<Paciente>> PegarPacientesAsync(bool status, string nomePrefixo, int pagina, int quantidadePorPagina);
+
+        public Task AdicionarPacienteAsync(Paciente paciente);
+
+        public Task AtualizarPacienteAsync(Paciente paciente);
+    }
+}
