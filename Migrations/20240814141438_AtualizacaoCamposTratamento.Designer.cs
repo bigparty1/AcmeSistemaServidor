83f503383be6196f00ce324b510542c7433f038b
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 11:15:24 -0300

    fix: Migration para atualização dos campos do Tratamento

--- Migrations/20240814141438_AtualizacaoCamposTratamento.Designer.cs
+++ Migrations/20240814141438_AtualizacaoCamposTratamento.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AcmeSistemaServidor.Data.Contexto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AcmeSistemaServidor.Migrations
 {
     [DbContext(typeof(AcmeSistemaContexto))]
-    partial class AcmeSistemaContextoModelSnapshot : ModelSnapshot
+    [Migration("20240814141438_AtualizacaoCamposTratamento")]
+    partial class AtualizacaoCamposTratamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240814141438_AtualizacaoCamposTratamento.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcmeSistemaServidor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizacaoCamposTratamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Tratamentos",
+                newName: "Ativo");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Tratamentos",
+                newName: "Data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Data",
+                table: "Tratamentos",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "Ativo",
+                table: "Tratamentos",
+                newName: "Status");
+        }
+    }
+}
