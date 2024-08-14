49edd7760fb22316d7f170211d582c8dd1486b69
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 09:40:47 -0300

    feat: Adicionar interface do repositório do tratamento

---
+++ Repositorio/Interface/ITratamentoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using AcmeSistemaServidor.Data.Models;
+using AcmeSistemaServidor.Data.Models.Entities;
+
+namespace AcmeSistemaServidor.Repositorio.Interface
+{
+    public interface ITratamentoRepositorio
+    {
+        public Task<ResultadoPaginado<Tratamento>> PegarTratamentosAsync(bool status, int pagina, int quantidadePorPagina);
+
+        public Task AdicionarTratamentoAsync(Tratamento tratamento);
+
+        public Task AtualizarTratamentoAsync(Tratamento tratamento);
+    }
+}
