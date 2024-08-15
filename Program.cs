207c74908bcbc199925937baeabf40674877afc3
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-15 01:27:41 -0300

    chore: Usar Swagger em produção

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace AcmeSistemaServidor
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() && app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
